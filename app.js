8b0c5415c318f3c118c6662a6ae601c5c540fd66
jdnovar <jdnovar@unal.edu.co>
2021-10-27 02:16:23 -0400

    Commit messagesdasdsa3

--- app.js
+++ app.js
@@ -123,10 +123,10 @@ app.use(session({
   // proxy: true,
   cookie: {
     httpOnly: true,
-    domain: 'lukapetshop.com.co',
+    domain: '.lukapetshop.com.co',
     // secure: true, 
-    maxAge: config.get('session.max_age')
-    // sameSite: 'none'
+    maxAge: config.get('session.max_age'),
+    sameSite: 'lax'
 
     // Set this to true only after veniqa has a ssl enabled site
 
